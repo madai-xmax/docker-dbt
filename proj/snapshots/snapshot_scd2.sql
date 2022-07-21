1f26c497d78fb4c57261c4c7edb9baa6674b2c04
Sergii Madai <sergii.madai@gmail.com>
2022-07-21 17:18:06 +0300

    changed snpshot strategy type

--- proj/snapshots/snapshot_scd2.sql
+++ proj/snapshots/snapshot_scd2.sql
@@ -5,10 +5,10 @@
       target_database= 'DEMO',
       target_schema= 'DW',
       unique_key= 'ID',
-      strategy= 'check',
-       check_cols= ['Price']
+       strategy='timestamp',
+       updated_at='UPDATED_AT'
        )
 }}
-select {{ dbt_utils.surrogate_key(['Id','Price']) }} as product_key,ProductName,Price,IsActive,CategoryName,Id
+select {{ dbt_utils.surrogate_key(['Id','Price']) }} as product_key,ProductName,Price,IsActive,CategoryName,Id,UPDATED_AT
 from DEMO.STAGE.PRODUCTS
 {% endsnapshot %}
